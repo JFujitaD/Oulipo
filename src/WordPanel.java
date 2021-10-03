fbb5b580bde20139a174217e550db87c7899e773
JFujitaD <fujitajulian@icloud.com>
2021-10-02 19:05:03 -0600

    Added a word panel

--- src/WordPanel.java
+++ src/WordPanel.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
 
 public class WordPanel extends JPanel{
 	private final int PANEL_WIDTH = 250;
@@ -13,13 +12,19 @@ public class WordPanel extends JPanel{
 	public WordPanel(ArrayList<String> words) {
 		this.setPreferredSize(new Dimension(PANEL_WIDTH, MyUtils.getPreferredSize().height));
 		
+		// Text area
 		JTextArea textArea = new JTextArea();
 		textArea.setFont(new Font("monospaced", Font.BOLD, 18));
+		textArea.append("----------");
 		for(String word : MyUtils.getSortedWords()) {
 			textArea.append("\n" + word);
 		}
 		
 		textArea.setPreferredSize(new Dimension(PANEL_WIDTH, MyUtils.getPreferredSize().height));
 		this.add(textArea);
+		
+		// Scroll pane
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		this.add(scrollPane);
 	}
 }
